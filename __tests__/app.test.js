78e4161477383b34dd10905bef7836bf0eb0d1fc
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-05-28 16:59:15 +0100

    Completes getArticles basic endpoint and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,16 +41,35 @@ describe("GET /api/topics", () => {
 });
 
 describe("GET /api/articles", () => {
-    test("200 returns an array of all article objects sorted by descending date order", () => {
+    test("200 returns an array of all article objects", () => {
         return request(app)
             .get("/api/articles")
             .expect(200)
-            // .expect("Content-Type", "application/json; charset=utf-8")
-            .then(({ body }) => {
+            .expect("Content-Type", "application/json; charset=utf-8")
+            .then(( { body } ) => {
                 const { articles } = body;
-                //console.log(articles);
+                expect(articles.length).toBe(13);
+                articles.forEach((article) => {
+                    expect(typeof article.author).toBe("string");
+                    expect(typeof article.title).toBe("string");
+                    expect(typeof article.article_id).toBe("number");
+                    expect(typeof article.topic).toBe("string");
+                    expect(typeof article.created_at).toBe("string");
+                    expect(typeof article.votes).toBe("number");
+                    expect(typeof article.article_img_url).toBe("string");
+                    expect(typeof article.comment_count).toBe("string");
+                })
             });
     });
+    test("200 returns sorted by descending date order", () => {
+        return request(app)
+            .get("/api/articles")
+            .expect(200)
+            .then(( { body } ) => {
+                const { articles } = body;
+                expect(articles).toBeSortedBy("created_at", { descending: true } );
+            })
+    })
 });
 
 describe("GET /api/articles/:article_id", () => {
