5ad54fa86185c8ffa6a5dd75efc4050d31ed2ceb
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-05-28 13:37:30 +0100

    Creates the api endpoint to get all endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,15 +5,29 @@ const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed.js");
 const testData = require("../db/data/test-data");
 
+const currentEndpointsTest = require("../endpoints.json");
+
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("GET /api", () => {
+    test("200 responds with object containing available endpoints", () => {
+        return request(app)
+        .get("/api")
+        .expect(200)
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .then(( { body } ) => {
+            expect(JSON.stringify(body)).toBe(JSON.stringify(currentEndpointsTest));
+        })
+    })
+})
 
 describe("GET /api/topics", () => {
     test("200 returns an array of topic objects", () => {
         return request(app)
             .get("/api/topics")
             .expect(200)
-            .then(({ body }) => {
+            .then(( { body } ) => {
                 const { topics } = body;
                 expect(topics.length).toBe(3);
                 topics.forEach((topic) => {
