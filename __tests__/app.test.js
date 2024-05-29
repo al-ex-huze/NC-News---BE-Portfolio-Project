85cf63d6af5e0919b12aaf089dbdbe23c9e4a67c
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-05-29 09:50:57 +0100

    Refactors and removes unwanted tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -77,31 +77,17 @@ describe("GET /api/articles/:article_id", () => {
         return request(app)
             .get("/api/articles/1")
             .expect(200)
-            .expect("Content-Type", "application/json; charset=utf-8")
             .then(({ body }) => {
                 const { article } = body;
-                expect(typeof article.author).toBe("string");
-                expect(article.author).toEqual("butter_bridge");
-                expect(typeof article.title).toBe("string");
-                expect(article.title).toEqual(
-                    "Living in the shadow of a great man"
-                );
-                expect(typeof article.article_id).toBe("number");
                 expect(article.article_id).toEqual(1);
+                expect(typeof article.author).toBe("string");
+                expect(typeof article.title).toBe("string");
+                expect(typeof article.article_id).toBe("number");
                 expect(typeof article.body).toBe("string");
-                expect(article.body).toEqual(
-                    "I find this existence challenging"
-                );
                 expect(typeof article.topic).toBe("string");
-                expect(article.topic).toEqual("mitch");
                 expect(typeof article.created_at).toBe("string");
-                expect(article.created_at).toEqual("2020-07-09T20:11:00.000Z");
                 expect(typeof article.votes).toBe("number");
-                expect(article.votes).toEqual(100);
-                expect(typeof article.article_img_url).toBe("string");
-                expect(article.article_img_url).toEqual(
-                    "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
-                );
+                expect(typeof article.article_img_url).toBe("string");                
             });
     });
 });
